f6dc63d7f156d6df2e19d5992bef9e35b8c98515
ChocolateCharlie <aurore.amrit@gmail.com>
2021-08-17 17:55:37 +0200

    Adds << operator to Coordinates class
    Adds show private method to Coordinates class
    Updates includes
    Refactors comments

--- Coordinates.cpp
+++ Coordinates.cpp
@@ -1,12 +1,32 @@
 /* ************************************************************************* */
 /*                                                    THE COGMOL-XYZ PROJECT */
-/* Header file                                                   COORDINATES */
+/* Coordinates.cpp                                               COORDINATES */
 /* By: ChocolateCharlie                                                      */
 /* Updated: 2021-08-17                                                       */
 /* ************************************************************************* */
 
 #include "Coordinates.h"
 
+/* ************************************************************************* */
+/*                              PUBLIC METHODS                               */
+/* ************************************************************************* */
 // CONSTRUCTOR
 Coordinates::Coordinates() : m_x(0.L), m_y(0.L), m_z(0.L) {}
 Coordinates::Coordinates(const long double &x, const long double &y, const long double &z) : m_x(x), m_y(y), m_z(z) {}
+
+/* ************************************************************************* */
+/*                              PRIVATE METHODS                              */
+/* ************************************************************************* */
+// CONSTANT METHODS
+void Coordinates::show(std::ostream &stream) const {
+    stream << std::setprecision(std::numeric_limits<long double>::digits10 + 1) << "(" << m_x << ";" << m_y << ";" << m_z << ")";
+}
+
+/* ************************************************************************* */
+/*                              FRIEND METHODS                               */
+/* ************************************************************************* */
+// OPERATORS
+std::ostream& operator<<(std::ostream &stream, const Coordinates &c) {
+    c.show();
+    return(stream);
+}
--- Coordinates.h
+++ Coordinates.h
@@ -1,6 +1,6 @@
 /* ************************************************************************* */
 /*                                                    THE COGMOL-XYZ PROJECT */
-/* Header file                                                   COORDINATES */
+/* Coordinates.h                                                 COORDINATES */
 /* By: ChocolateCharlie                                                      */
 /* Updated: 2021-08-17                                                       */
 /* ************************************************************************* */
@@ -8,15 +8,23 @@
 #ifndef COORDINATES_H_INCLUDED
 #define COORDINATES_H_INCLUDED
 
+#include <iomanip>
+#include <iostream>
+#include <limits>
+
 class Coordinates {
     public:
         Coordinates();
         Coordinates(const long double &x = 0.L, const long double &y = 0.L, const long double &z = 0.L);
 
     private:
+        void show(std::ostream &stream = std::cout) const;
+
         long double m_x;
         long double m_y;
         long double m_z;
+
+    friend std::ostream& operator<<(std::ostream &stream, const Coordinates &c);
 };
 
 #endif // COORDINATES_H_INCLUDED
